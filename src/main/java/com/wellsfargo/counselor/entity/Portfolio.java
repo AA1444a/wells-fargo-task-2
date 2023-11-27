3db2c0f77c949f35de1390c016d180b773d8447e
AA1444a <152125547+AA1444a@users.noreply.github.com>
2023-11-26 20:42:57 -0600

    Update Portfolio.java

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,1 +1,43 @@
+package com.wellsfargo.counselor.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String creationDate;
+
+   
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Long getPortfolioId() {
+        return portfiolioId;
+    }
+   
+    public String getcreationDate() {
+        return creationDate;
+    }
+
+    public void setcreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    }
+
+
+}
