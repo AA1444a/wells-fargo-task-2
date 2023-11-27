33ecd5d5b3f3f2bc93675911999dbf0defb79dc9
AA1444a <152125547+AA1444a@users.noreply.github.com>
2023-11-26 21:20:07 -0600

    Update Security.java

--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -1,1 +1,88 @@
+package com.wellsfargo.counselor.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private float purchasePrice;
+
+    @Column(nullable = false)
+    private String purchaseDate;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    protected Security() {
+
+    }
+
+    public Security(String name, String category, Float purchasePrice, String purchaseDate, Integer quantity) {
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getcategory() {
+        return category;
+    }
+
+    public void setcategory(String category) {
+        this.category = category;
+    }
+
+    public Float getpurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setpurchasePrice(Float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+   public String getpurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setpurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+
+}
